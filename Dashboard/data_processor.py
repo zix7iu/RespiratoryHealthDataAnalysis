3dcde8c5bdb65857676db7f1b40dfa39f6d7f3eb
Lydia Liu <lydialiu033@gmail.com>
2024-11-07 13:26:10 -0800

    remove unnecessary files

--- Dashboard/data_processor.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def load_data():
-    # Load and process your data
-    data = pd.read_csv('path_to_your_data.csv')
-    # Process and prepare data to send to frontend
-    return data.to_dict(orient='records')
